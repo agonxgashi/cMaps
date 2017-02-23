68428e8b1367c67c4e50ed539178a6b6494b34ea
Agon Gashi <agongashi318@gmail.com>
2017-02-23 11:00:26 +0100

    FirstClasses

---
+++ csMaps/csMaps/csMaps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace csMaps
+{
+    class csMaps
+    {
+        [JsonProperty(PropertyName = "results")]
+        public List<Result> results = new List<Result>();
+
+        [JsonProperty(PropertyName = "status")]
+        public string status { get; set; }
+    }
+
+    class Result
+    {
+        [JsonProperty(PropertyName = "geometry")]
+        public Geometry geometry = new Geometry();
+    }
+
+    class Geometry
+    {
+        [JsonProperty(PropertyName = "location")]
+        public Location Location = new Location();
+    }
+
+    class Location
+    {
+        [JsonProperty(PropertyName = "lat")]
+        public string lat { get; set; }
+
+        [JsonProperty(PropertyName = "lng")]
+        public string lng { get; set; }
+    }
+}
