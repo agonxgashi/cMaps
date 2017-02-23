e99e63dec0446c97608d0266337474996d16bc00
Agon Gashi <agongashi318@gmail.com>
2017-02-23 11:14:05 +0100

    csMap Class population

--- csMaps/csMaps/csMaps.cs
+++ csMaps/csMaps/csMaps.cs
@@ -7,7 +7,7 @@ using Newtonsoft.Json;
 
 namespace csMaps
 {
-    class csMaps
+    class csMap
     {
         [JsonProperty(PropertyName = "results")]
         public List<Result> results = new List<Result>();
@@ -18,22 +18,71 @@ namespace csMaps
 
     class Result
     {
+        [JsonProperty(PropertyName = "address_components")]
+        public List<AddressComponents> AddressComponents = new List<AddressComponents>();
+
+        [JsonProperty(PropertyName = "formatted_address")]
+        public string FormattedAddress { get; set; }
+
         [JsonProperty(PropertyName = "geometry")]
         public Geometry geometry = new Geometry();
+
+        [JsonProperty(PropertyName = "partial_match")]
+        public bool PartialMatch { get; set; }
+
+        [JsonProperty(PropertyName = "place_id")]
+        public string PlaceId { get; set; }
+
+        [JsonProperty(PropertyName = "types")]
+        public List<string> Types = new List<string>();
+        
     }
+
+    class AddressComponents
+    {
+        [JsonProperty(PropertyName = "long_name")]
+        public string LongName { get; set; }
+        
+        [JsonProperty(PropertyName = "short_name")]
+        public string ShortName { get; set; }
+
+        [JsonProperty(PropertyName = "types")]
+        public List<string> Types = new List<string>();
+
+    }
+
+
 
     class Geometry
     {
+        [JsonProperty(PropertyName = "bounds")]
+        public Bounds Bounds { get; set; }
+
         [JsonProperty(PropertyName = "location")]
-        public Location Location = new Location();
+        public LatLng Location = new LatLng();
+
+        [JsonProperty(PropertyName = "location_type")]
+        public string LocationType { get; set; }
+
+        [JsonProperty(PropertyName = "viewport")]
+        public Bounds Viewport { get; set; }
     }
 
-    class Location
+    class Bounds
+    {
+        [JsonProperty(PropertyName = "northeast")]
+        public LatLng Northeast { get; set; }
+
+        [JsonProperty(PropertyName = "southwest")]
+        public LatLng Southwest { get; set; }
+    }
+
+    class LatLng
     {
         [JsonProperty(PropertyName = "lat")]
-        public string lat { get; set; }
+        public string Latitude { get; set; }
 
         [JsonProperty(PropertyName = "lng")]
-        public string lng { get; set; }
+        public string Longitude { get; set; }
     }
 }
